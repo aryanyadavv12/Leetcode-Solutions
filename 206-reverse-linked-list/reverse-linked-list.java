c9d04aac66496c94102d691a7fcbc59faa50b196
Aryan Yadav <py7355384705@gmail.com>
2025-12-11 20:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (40.25%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,14 @@
+class Solution {
+    //Aryan
+    public ListNode reverseList(ListNode head) {
+    ListNode prev=null;
+    ListNode curr=head;
+    while(curr!=null){
+        ListNode  temp=curr.next;    //step1
+        curr.next=prev;              //step2
+         prev = curr;                //step3
+        curr=temp;                   //step4
+    }
+    return prev;
+    }
+}
