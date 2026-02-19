1c1f7509ed19108d4a4f894c87b1a3ef3364ed37
Aryan Yadav <py7355384705@gmail.com>
2026-02-19 19:31:10 +0530

    Time: 9 ms (94.67%) | Memory: 47 MB (9.24%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    //Aryan
+    public int countBinarySubstrings(String s) {
+        int n = s.length();
+        int[] blocks = new int[n];
+        int idx = 0;
+        
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                blocks[idx++] = count;
+                count = 1;
+            }
+        }
+        blocks[idx++] = count;
+        
+        int res = 0;
+        for (int i = 0; i < idx - 1; i++) {
+            res += Math.min(blocks[i], blocks[i + 1]);
+        }
+        
+        return res;
+    }
+}
