2482b156eeeebb2bfa85127b4e0226ac10827ada
Aryan Yadav <py7355384705@gmail.com>
2026-02-22 11:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (30.35%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,23 @@
+class Solution {
+    //Aryan
+    public int binaryGap(int n) {
+        int maxGap = 0;
+        int currentGap = -1;
+
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                if (currentGap != -1) {
+                    maxGap = Math.max(maxGap, currentGap);
+                }
+                currentGap = 1;
+            } else {
+                if (currentGap != -1) {
+                    currentGap++;
+                }
+            }
+            n >>= 1;
+        }
+
+        return maxGap;
+    }
+}
